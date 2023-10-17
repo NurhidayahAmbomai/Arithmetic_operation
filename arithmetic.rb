5ff4ac0715a6867c1cfc8a6f535eaea7f6bef824
Nurhidayah Ambomai <hidayahambomai@gmail.com>
2023-10-17 10:51:04 +0800

    define method of addition, substraction, multiply and division

--- arithmetic.rb
+++ arithmetic.rb
@@ -1,3 +1,19 @@
+def add(x, y)
+    x + y
+end
+
+def substract(x, y)
+    x - y
+end
+
+def multiply(x, y)
+    x * y
+end
+
+def division(x, y)
+    x / y
+end
+
 puts "Hello welcome to this arithmetic_operation program\nPuts your name"
 name = gets.chomp
 
@@ -8,7 +24,6 @@ first = gets.chomp.to_i
 
 puts "Enter second number"
 second = gets.chomp.to_i
-
-result = first + second 
-
+ 
+result = add(first, second)
 puts "The result is #{result}" 
