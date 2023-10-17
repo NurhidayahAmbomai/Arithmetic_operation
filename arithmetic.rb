18e4bf76b538c252d52ba2aa82499491e2f5c4cd
Nurhidayah Ambomai <hidayahambomai@gmail.com>
2023-10-17 10:23:34 +0800

    Do changes to know the overview of the program

--- arithmetic.rb
+++ arithmetic.rb
@@ -1,18 +1,2 @@
 puts "Hello whats your name?"
 name = gets.chomp
-
-def add(x,y)
-    x + y
-end
-
-def substract(x,y)
-    x - y
-end
-
-def multiply(x,y)
-    x * y
-end
-
-def division(x,y)
-    x / y
-end
