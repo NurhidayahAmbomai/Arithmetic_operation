8401b45690883ea64279b9e053bf34857827d3b5
Nurhidayah Ambomai <hidayahambomai@gmail.com>
2023-10-17 11:45:04 +0800

    add case statements

--- arithmetic.rb
+++ arithmetic.rb
@@ -14,7 +14,7 @@ def division(x, y)
     x / y
 end
 
-puts "Hello welcome to this arithmetic_operation program\nPuts your name"
+puts "Hello welcome to this arithmetic_operation program \nPuts your name"
 name = gets.chomp
 
 puts "Thank you for choosing our program #{name}"
@@ -24,6 +24,28 @@ first = gets.chomp.to_i
 
 puts "Enter second number"
 second = gets.chomp.to_i
- 
-result = add(first, second)
-puts "The result is #{result}" 
+
+puts "Select arithmetic operation"
+puts "1. Addition \t2. Substraction \n3. Multiply \t4. Division"
+operation = gets.chomp.to_i
+result = case operation
+when 1
+    add(first, second)
+when 2
+    substract(first, second)
+when 3
+    multiply(first, second)
+when 4
+    division(first, second)
+else 
+    "Invalid operation"
+end
+
+if result == "Invalid operation"
+    puts "Try again"
+else
+    puts "The result is #{result}" 
+end
+
+
+
