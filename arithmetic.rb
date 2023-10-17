9fb28702d8303afd1c736698ceacb6db7aeb94a5
Nurhidayah Ambomai <hidayahambomai@gmail.com>
2023-10-17 10:36:40 +0800

    Add first number and second number

--- arithmetic.rb
+++ arithmetic.rb
@@ -2,3 +2,13 @@ puts "Hello welcome to this arithmetic_operation program\nPuts your name"
 name = gets.chomp
 
 puts "Thank you for choosing our program #{name}"
+
+puts "Enter first number"
+first = gets.chomp.to_i
+
+puts "Enter second number"
+second = gets.chomp.to_i
+
+result = first + second 
+
+puts "The result is #{result}" 
